42f35c442ca054cc2396f1546bbd6e50ab3e45f6
Samir31 <Samilr.sg2@gmail.com>
2022-08-08 00:44:22 -0400

    -Add funtion Casa

--- PruebaGit.java
+++ PruebaGit.java
@@ -1,5 +1,12 @@
 public class PruebaGit {
     public static void main(String[] args) {
         System.out.println('Hello world');
+
+        public String Casa(Guaricano){
+            return Guaricano;
+        }
+
+        Casa("Anacaona");
     }
+
 }
